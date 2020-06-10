276ffae55655d85a64bce752cd3aaea88ad59f72
jian.liu <jian.liu@genlot.com>
2020-06-10 11:31:29 +0800

    git:add 算法训练反转链表

---
+++ src/main/java/io/algorithm/collection/ReverseList/ListNode.java
@@ -0,0 +1,13 @@
+package io.algorithm.collection.ReverseList;
+
+/**
+ * @创建人 thomas_liu
+ * @创建时间 2020/6/10 10:20
+ * @描述 链表
+ */
+//struct ListNode {
+//        int val;
+//        ListNode *next;
+//        ListNode(int x) : val(x), next(NULL) {}
+//
+//}
