5b73e42d67bf23011de0d835d938e98c790994d7
liujian <1690296356@qq.com>
2021-06-02 11:45:07 +0800

    二叉树深度计算，BFS解法
    广度优先搜索

--- src/main/java/io/algorithm/binary/tree/max/depth/Solution.java
+++ src/main/java/io/algorithm/binary/tree/max/depth/Solution.java
@@ -1,15 +1,41 @@
 package io.algorithm.binary.tree.max.depth;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
 
+//    public int maxDepth(TreeNode root){
+//        if(root == null){
+//            return 0;
+//        }
+//        int left = maxDepth(root.left);
+//        int right = maxDepth(root.right);
+//        return Math.max(left, right)+1;
+//    }
+
+
     public int maxDepth(TreeNode root){
+
         if(root == null){
             return 0;
         }
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        return Math.max(left, right)+1;
-    }
 
+        int level = 0;
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            level++;
+            for(int i =0; i< size; i++){
+                TreeNode node = queue.remove();
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+
+        }
+        return level;
+    }
 
 }
