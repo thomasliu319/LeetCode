7cdfe080b832edf05c3a379facdc288eebddfad9
liujian <1690296356@qq.com>
2021-06-02 11:00:37 +0800

    二叉树的深度
    常规解法

---
+++ src/main/java/io/algorithm/binary/tree/max/depth/Solution.java
@@ -0,0 +1,15 @@
+package io.algorithm.binary.tree.max.depth;
+
+public class Solution {
+
+    public int maxDepth(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right)+1;
+    }
+
+
+}
