dbe80304fa22785618446ff0ded0a0b24b4ad354
liujian <1690296356@qq.com>
2021-06-02 11:55:15 +0800

    二叉树的深度，DFS解法
    深度优先搜索

--- src/main/java/io/algorithm/binary/tree/max/depth/Solution.java
+++ src/main/java/io/algorithm/binary/tree/max/depth/Solution.java
@@ -14,28 +14,48 @@ public class Solution {
 //        return Math.max(left, right)+1;
 //    }
 
+//BFS
+//    public int maxDepth(TreeNode root){
+//
+//        if(root == null){
+//            return 0;
+//        }
+//
+//        int level = 0;
+//
+//        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+//        queue.add(root);
+//        while(!queue.isEmpty()){
+//            int size = queue.size();
+//            level++;
+//            for(int i =0; i< size; i++){
+//                TreeNode node = queue.remove();
+//                if(node.left != null) queue.add(node.left);
+//                if(node.right != null) queue.add(node.right);
+//            }
+//
+//        }
+//        return level;
+//    }
+
+//DFS
+    int maxLevel = 0;
 
     public int maxDepth(TreeNode root){
-
         if(root == null){
             return 0;
         }
+        dfs(root, 1);
+        return  maxLevel;
+    }
 
-        int level = 0;
-
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        queue.add(root);
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            level++;
-            for(int i =0; i< size; i++){
-                TreeNode node = queue.remove();
-                if(node.left != null) queue.add(node.left);
-                if(node.right != null) queue.add(node.right);
-            }
-
+    public void dfs(TreeNode root, int level){
+        if(root == null){
+            return;
         }
-        return level;
+        if(level > maxLevel) maxLevel = level;
+        dfs(root.left, level+1);
+        dfs(root.right, level+1);
     }
 
 }
