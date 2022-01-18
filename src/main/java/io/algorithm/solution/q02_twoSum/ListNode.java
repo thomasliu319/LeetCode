481afec19dadc0bce882067ca8b0cc0c4f0dab32
刘剑 <jian.liu@casstime.com>
2022-01-18 20:42:44 +0800

    q02_两数相加

---
+++ src/main/java/io/algorithm/solution/q02_twoSum/ListNode.java
@@ -0,0 +1,24 @@
+package io.algorithm.solution.q02_twoSum;
+
+/**
+ * @Author: jian.liu
+ * @Description //TODO
+ * @Date: 2022/1/18 16:39
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}
