7d81ab0420572fcdcaad3fffdc386eeaefd68171
刘剑 <jian.liu@casstime.com>
2022-01-27 11:13:55 +0800

    q21合并两个有序链表

---
+++ src/main/java/io/algorithm/solution/q21_merge_two_sorted_lists/ListNode.java
@@ -0,0 +1,24 @@
+package io.algorithm.solution.q21_merge_two_sorted_lists;
+
+/**
+ * @Author: jian.liu
+ * @Description //链表节点
+ * @Date: 2022/1/26 16:30
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ src/main/java/io/algorithm/solution/q21_merge_two_sorted_lists/Solution.java
@@ -0,0 +1,29 @@
+package io.algorithm.solution.q21_merge_two_sorted_lists;
+
+/**
+ * @Author: jian.liu
+ * @Description //合并两个有序链表
+ * @Date: 2022/1/26 16:27
+ */
+public class Solution {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        if(list1 == null){
+            return list2;
+        }
+
+        if(list2 == null){
+            return list2;
+        }
+
+        if(list1.next.val < list2.next.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        }else{
+            list2.next = mergeTwoLists(list1, list2.next);
+            return  list2;
+        }
+
+    }
+}
