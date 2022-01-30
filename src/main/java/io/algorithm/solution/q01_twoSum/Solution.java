375e111bc6f842561d9f8610f1896e69af6f114d
刘剑 <jian.liu@casstime.com>
2022-01-30 16:31:22 +0800

    q01两数之和

--- src/main/java/io/algorithm/solution/q01_twoSum/Solution.java
+++ src/main/java/io/algorithm/solution/q01_twoSum/Solution.java
@@ -1,9 +1,9 @@
-package io.algorithm.solution.sum;
+package io.algorithm.solution.q01_twoSum;
 
 import java.util.HashMap;
 
 /**
- * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * 给定一个整数数组 nums和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那两个整数，并返回它们的数组下标。
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
  * 你可以按任意顺序返回答案。
  *
@@ -17,11 +17,11 @@ public class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0; i< nums.length; i++){
-            if(map.containsKey(nums[i])){
-                return new int[]{map.get(nums[i]), i};
+            if(map.containsKey(target - nums[i])){
+                return new int[]{map.get(target - nums[i]), i};
             }
-            map.put(target - nums[i], i);
+            map.put(nums[i], i);
         }
-        return null;
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
