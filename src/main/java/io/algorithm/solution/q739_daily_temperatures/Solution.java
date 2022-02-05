debcf0b0fe44336b38d71d11324235357cf3f0ef
刘剑 <jian.liu@casstime.com>
2022-02-05 14:19:46 +0800

    q739每日气温

---
+++ src/main/java/io/algorithm/solution/q739_daily_temperatures/Solution.java
@@ -0,0 +1,32 @@
+package io.algorithm.solution.q739_daily_temperatures;
+
+import java.util.Stack;
+
+/**
+ * @Author: jian.liu
+ * @Description //739. 每日温度
+ * @Date: 2022/2/5 14:08
+ */
+public class Solution {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        int length = temperatures.length;
+
+        int[] ans = new int[length];
+
+        for(int i=0; i<length; i++){
+            while(!stack.isEmpty()&& temperatures[i]> temperatures[stack.peek()]){
+                int pre = stack.pop();
+                ans[pre] = i - pre;
+            }
+            stack.add(i);
+        }
+
+        return ans;
+    }
+
+
+}
