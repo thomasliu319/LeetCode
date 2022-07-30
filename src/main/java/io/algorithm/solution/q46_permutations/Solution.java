2bc550f3c939b9808c0f42cfb3c3acdbded350d5
liujian <1690296356@qq.com>
2022-07-30 17:34:04 +0800

    全排列算法

---
+++ src/main/java/io/algorithm/solution/q46_permutations/Solution.java
@@ -0,0 +1,15 @@
+package io.algorithm.solution.q46_permutations;
+
+import java.util.List;
+
+/**
+ * @author liujian
+ * @date 2022/7/30 17:29
+ * @description
+ */
+public class Solution {
+
+    public List<List<Integer>> permute(int[] nums) {
+
+    }
+}
